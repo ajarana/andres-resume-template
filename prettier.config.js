acc958a92bdfa8ef6ca79589911e0d505a3e8605
Andres <andresredans@gmail.com>
2025-09-22 02:29:48 -0400

    Prettier config updates

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,22 @@
-const config = {};
+const config = {
+  experimentalTernaries: true,
+  experimentalOperatorPosition: "end",
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  objectWrap: "preserve",
+  bracketSameLine: false,
+  arrowParens: "always",
+  htmlWhitespaceSensitivity: "strict",
+  endOfLine: "auto",
+  embeddedLanguageFormatting: "off",
+  singleAttributePerLine: false,
+};
 
 module.exports = config;
