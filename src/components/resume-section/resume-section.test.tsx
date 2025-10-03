f3f785dcff15aba9adeadaa95d00c85954c07426
Andres <andresredans@gmail.com>
2025-10-02 22:53:59 -0400

    ResumeSection test

---
+++ src/components/resume-section/resume-section.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import ResumeSection from "./resume-section";
+
+test("ResumeSection renders a header and children", () => {
+  const title = "Personal Projects";
+  const childContent = "Child Content";
+
+  render(
+    <ResumeSection title={title}>
+      <>{childContent}</>
+    </ResumeSection>,
+  );
+
+  const headingElement = screen.getByRole("heading", {
+    name: title,
+    level: 2,
+  });
+  const childElementContent = screen.getByText(childContent);
+
+  expect(headingElement).toBeInTheDocument();
+  expect(childElementContent).toBeInTheDocument();
+});
